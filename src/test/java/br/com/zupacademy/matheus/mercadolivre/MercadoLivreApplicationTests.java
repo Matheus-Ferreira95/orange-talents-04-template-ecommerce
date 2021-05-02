a18a4a353908f6c1d7fa4daf3e1e5c93bc0f7422
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-05-02 15:26:15 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/matheus/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.matheus.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
